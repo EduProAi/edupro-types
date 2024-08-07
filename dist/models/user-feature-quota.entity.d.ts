9db5c9a479b4ded93eee368dfa689f74b2d41f1b
terrytz <tz@terryzhang.com>
2024-08-07 16:14:05 +0800

    user feature quota migrate away from plan/subscriptions

--- dist/models/user-feature-quota.entity.d.ts
+++ dist/models/user-feature-quota.entity.d.ts
@@ -2,6 +2,4 @@ import { Feature } from "./feature.entity";
 export type UserFeatureQuota = Partial<Feature> & {
     feature_handle: string;
     quota: number;
-    remaining_quota?: number;
-    plan_quota?: number;
 };
