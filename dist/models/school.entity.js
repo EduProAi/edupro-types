d50c1b916160d65c08edd367df44fdbe6cee773a
terrytz <tz@terryzhang.com>
2024-08-13 22:43:08 +0800

    full school db interface mapped

--- dist/models/school.entity.js
+++ dist/models/school.entity.js
@@ -1,2 +1,48 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.YearType = exports.CampusSetting = exports.SchoolType = exports.Requirement = exports.Degree = void 0;
+// Added enums
+var Degree;
+(function (Degree) {
+    Degree["LESS_THAN_ONE_YEAR"] = "LESS_THAN_ONE_YEAR";
+    Degree["LESS_THAN_TWO_YEAR"] = "LESS_THAN_TWO_YEAR";
+    Degree["TWO_YEARS_PLUS"] = "TWO_YEARS_PLUS";
+    Degree["ASSOCIATE"] = "ASSOCIATE";
+    Degree["BACHELOR"] = "BACHELOR";
+    Degree["POST_BACHELOR"] = "POST_BACHELOR";
+    Degree["MASTER"] = "MASTER";
+    Degree["POST_MASTER"] = "POST_MASTER";
+    Degree["DOCTOR"] = "DOCTOR";
+    Degree["POST_DOCTOR"] = "POST_DOCTOR";
+    Degree["PROFESSIONAL"] = "PROFESSIONAL";
+    Degree["OTHER"] = "OTHER";
+})(Degree || (exports.Degree = Degree = {}));
+var Requirement;
+(function (Requirement) {
+    Requirement["REQUIRED"] = "REQUIRED";
+    Requirement["CONSIDERED"] = "CONSIDERED";
+    Requirement["NOT_REQUIRED"] = "NOT_REQUIRED";
+    Requirement["UNSPECIFIED"] = "UNSPECIFIED";
+})(Requirement || (exports.Requirement = Requirement = {}));
+var SchoolType;
+(function (SchoolType) {
+    SchoolType["PUBLIC"] = "PUBLIC";
+    SchoolType["PRIVATE"] = "PRIVATE";
+    SchoolType["FOR_PROFIT"] = "FOR_PROFIT";
+    SchoolType["OTHER"] = "OTHER";
+})(SchoolType || (exports.SchoolType = SchoolType = {}));
+var CampusSetting;
+(function (CampusSetting) {
+    CampusSetting["URBAN"] = "URBAN";
+    CampusSetting["SUBURBAN"] = "SUBURBAN";
+    CampusSetting["TOWN"] = "TOWN";
+    CampusSetting["RURAL"] = "RURAL";
+    CampusSetting["OTHER"] = "OTHER";
+})(CampusSetting || (exports.CampusSetting = CampusSetting = {}));
+var YearType;
+(function (YearType) {
+    YearType["TWO_YEAR"] = "TWO_YEAR";
+    YearType["FOUR_YEAR"] = "FOUR_YEAR";
+    YearType["POST_GRADUATE"] = "POST_GRADUATE";
+    YearType["OTHER"] = "OTHER";
+})(YearType || (exports.YearType = YearType = {}));
--- src/models/school.entity.ts
+++ src/models/school.entity.ts
@@ -6,13 +6,138 @@ export interface SchoolSimple {
   region_cn_name?: string;
 }
 
-// the updated school entity based from database
+// Updated School interface to match the Prisma schema
 export interface School {
-  id: number;
-  name?: string;
+  id: string; // Changed to string to match the Prisma schema
+  name: string; // Made required
   name_cn?: string;
   description?: string;
   description_cn?: string;
   us_news_ranking?: number;
   qs_ranking?: number;
+  country?: string;
+  ipeds_id?: string; // Added
+  ope_id?: string; // Added
+
+  // Added related entities
+  basic_info?: BasicInfo;
+  website?: Website;
+  cost?: Cost;
+  stats?: Stats;
+  degrees?: SchoolDegree[];
+  majors?: Major[];
+  admission_profile?: AdmissionProfile;
+}
+
+// Added new interfaces for related entities
+
+export interface BasicInfo {
+  school_id: string;
+  address?: string;
+  phone?: string;
+  year_type?: YearType;
+  school_type?: SchoolType;
+  campus_setting?: CampusSetting;
+  campus_housing?: boolean;
+  student_to_faculty_ratio?: number;
+}
+
+export interface Website {
+  school_id: string;
+  homepage?: string;
+  admission?: string;
+  application?: string;
+  financial_aid?: string;
+  mission_statement?: string;
+}
+
+export interface Cost {
+  school_id: string;
+  tuition_regular?: number;
+  tuition_out_of_state?: number;
+  tuition_in_state?: number;
+  tuition_international?: number;
+  books_and_supplies?: number;
+  food_and_housing?: number;
+  other_expenses?: number;
+}
+
+export interface Stats {
+  school_id: string;
+  total_enrollment?: number;
+  undergraduate_enrollment?: number;
+  graduate_enrollment?: number;
+  number_of_applicants?: number;
+  percent_admitted?: number;
+  percent_admitted_who_enrolled?: number;
+  percent_students_submitted_sat?: number;
+  percent_students_submitted_act?: number;
+}
+
+export interface SchoolDegree {
+  id: string;
+  school_id: string;
+  degree: Degree;
+}
+
+export interface Major {
+  id: string;
+  school_id: string;
+  name: string;
+  name_cn?: string;
+  description?: string;
+  background_requirement?: string;
+  interview_requirement?: string;
+  tuition?: number;
+  portfolio_requirement?: string;
+  admission_profile?: AdmissionProfile;
+}
+
+export interface AdmissionProfile {
+  // ... (Add all fields from the Prisma schema)
+}
+
+// Added enums
+export enum Degree {
+  LESS_THAN_ONE_YEAR = "LESS_THAN_ONE_YEAR",
+  LESS_THAN_TWO_YEAR = "LESS_THAN_TWO_YEAR",
+  TWO_YEARS_PLUS = "TWO_YEARS_PLUS",
+  ASSOCIATE = "ASSOCIATE",
+  BACHELOR = "BACHELOR",
+  POST_BACHELOR = "POST_BACHELOR",
+  MASTER = "MASTER",
+  POST_MASTER = "POST_MASTER",
+  DOCTOR = "DOCTOR",
+  POST_DOCTOR = "POST_DOCTOR",
+  PROFESSIONAL = "PROFESSIONAL",
+  OTHER = "OTHER",
+}
+
+export enum Requirement {
+  REQUIRED = "REQUIRED",
+  CONSIDERED = "CONSIDERED",
+  NOT_REQUIRED = "NOT_REQUIRED",
+  UNSPECIFIED = "UNSPECIFIED",
+}
+
+export enum SchoolType {
+  PUBLIC = "PUBLIC",
+  PRIVATE = "PRIVATE",
+  FOR_PROFIT = "FOR_PROFIT",
+  OTHER = "OTHER",
+}
+
+export enum CampusSetting {
+  URBAN = "URBAN",
+  SUBURBAN = "SUBURBAN",
+  TOWN = "TOWN",
+  RURAL = "RURAL",
+  OTHER = "OTHER",
+}
+
+export enum YearType {
+  TWO_YEAR = "TWO_YEAR",
+  FOUR_YEAR = "FOUR_YEAR",
+  POST_GRADUATE = "POST_GRADUATE",
+  OTHER = "OTHER",
 }
